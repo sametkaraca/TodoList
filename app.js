439b1af58e13b09d433ba2c6abf32947ebea2d05
Samet Karaca <skaraca@onit.local>
2019-03-11 14:20:43 +0100

    Understanding Templating

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express()
 const port=3000
 
 var items = ["Buy Food","Eat Food","Cook Food"]
+var workItems =[]
+var lavoroItems =[]
 
 app.set('view engine', 'ejs')
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"))  
+app.use(express.static("public"))  //createas static files
 
 app.get("/", function(req,res){
 
@@ -22,19 +23,48 @@ app.get("/", function(req,res){
 
     var day = today.toLocaleDateString("en-US", options)
 
-    res.render("list", {KindOfDay: day, newListItems: items})
+    res.render("list", {listTitle: day, newListItems: items})
 
 })
 
-//POST from signup
 app.post('/', function(req,res){
-    var item = req.body.newItem
 
-    items.push(item)
+    let item = req.body.newItem
 
-    res.redirect("/")
+    if (req.body.list === "Work") {
+        workItems.push(item)
+        res.redirect("/work")
+    } else{
+        items.push(item)
+        res.redirect("/")
+    }
 })
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem
+    workItems.push(item)
+    res.redirect("/work")
+})
+
+app.get("/about", function(req,res){
+    res.render("about")
+})
+
+
+
+
+
+
+
+
+
+
+
+//Lsitens port
 app.listen(port, function(){
     console.log("The server listens " + port)
 })
