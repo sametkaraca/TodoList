8821b90fdcfdd5257b88551d6513bc6349a471d8
Samet Karaca <skaraca@onit.local>
2019-03-11 10:54:08 +0100

    style

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var items = ["Buy Food","Eat Food","Cook Food"]
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))  
 
 app.get("/", function(req,res){
 
